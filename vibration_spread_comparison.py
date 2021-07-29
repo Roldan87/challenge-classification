45260e55f99f80ca47c0697ba96fcd037beb782e
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-07-29 18:24:33 +0200

    added function and visual: three boxplots in one

--- vibration_spread_comparison.py
+++ vibration_spread_comparison.py
@@ -58,6 +58,7 @@ def write_to_csv(spreads_df, axis):
 
 
 def save_boxplot(spreads_df: pd.DataFrame, axis: str):
+    plt.clf()  # clear buffer of matplotlib as to not overlap plots
     sns.boxplot(x='target', y='difference_spread', data=spreads_df)
     plt.ylim([-5, 30])
     plt.title('Vibration spreads on axis ' + axis)
@@ -66,16 +67,42 @@ def save_boxplot(spreads_df: pd.DataFrame, axis: str):
     savefig_name = 'vibration_spreads_' + axis + '.png'
     savegif_path = 'visuals\\' + savefig_name
     plt.savefig(savegif_path)
+    plt.clf()
+
+
+def save_aligned_box_plots(x_spreads, y_spreads, z_spreads):
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 10), sharey='all')
+    plt.ylim([-10, 30])
+    fig.suptitle('Vibration spread differencess on axes x, y, z', fontsize=17)
+    fig.text(0.5, 0.02, 'bad (0) or good (1) bearing', fontsize=13, ha='center')
+    plt.ylabel('differences between max_spread of a2 and a1')
+    fig.text(0.07, 0.5,
+         'difference between max_min_spread of test and control bearing',
+         fontsize=13,
+         rotation='vertical',
+         va='center')
+    x = sns.boxplot(ax=ax1, x='target', y='difference_spread', data=x_spreads, palette='Set2')
+    x.set(xlabel='x', ylabel=None)
+    y = sns.boxplot(ax=ax2, x='target', y='difference_spread', data=y_spreads, palette='Set2')
+    y.set(xlabel='y', ylabel=None)
+    z = sns.boxplot(ax=ax3, x='target', y='difference_spread', data=z_spreads, palette='Set2')
+    z.set(xlabel='z', ylabel=None)
+    savefig_name = 'vibration_spread_differences_on_all_axes.png'
+    savegif_path = 'visuals\\' + savefig_name
+    plt.savefig(savegif_path)
+    plt.clf()
 
 
 x_spreads = get_spreads_per_axis('x')
-write_to_csv(x_spreads, 'x')
-save_boxplot(x_spreads, 'x')
+# write_to_csv(x_spreads, 'x')
+# save_boxplot(x_spreads, 'x')
 
 y_spreads = get_spreads_per_axis('y')
-write_to_csv(y_spreads, 'y')
-save_boxplot(y_spreads, 'y')
+# write_to_csv(y_spreads, 'y')
+# save_boxplot(y_spreads, 'y')
 
 z_spreads = get_spreads_per_axis('z')
-write_to_csv(z_spreads, 'z')
-save_boxplot(z_spreads, 'z')
+# write_to_csv(z_spreads, 'z')
+# save_boxplot(z_spreads, 'z')
+
+# save_aligned_box_plots(x_spreads, y_spreads, z_spreads)
