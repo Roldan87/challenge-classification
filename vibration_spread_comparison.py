541a14a35f87b2539c135f0fed17830563fcbb4c
Maarten Van den Bulcke <maarten.vdbul@gmail.com>
2021-07-30 18:27:39 +0200

    new plots and extended vibration comparison code

--- vibration_spread_comparison.py
+++ vibration_spread_comparison.py
@@ -3,8 +3,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 bearing_signals = pd.read_csv(r'..\archive\bearing_signals.csv')
 bearing_classes = pd.read_csv(r'..\archive\bearing_classes.csv', sep=';')
+
+
+def split_dataframes_to_drop_hertz_outliers(df, df_classes):
+    hertz_outlier = df[df.hz > 100]
+    hertz_outlier = hertz_outlier.groupby('experiment_id', as_index=False).agg('mean')
+    df_limit_test_features = df[df.experiment_id.isin(hertz_outlier.experiment_id.values)]
+    df_signals_dropped_outliers = df.drop(
+        df[df.experiment_id.isin(df_limit_test_features.experiment_id.unique())].index)
+    df_limit_test_classes = df[df.index.isin(hertz_outlier.experiment_id.values)]
+    df_classes_dropped_outliers = df_classes.drop(
+    df_classes[df_classes.index.isin(df_limit_test_features.experiment_id.unique())].index)
+
+    return df_signals_dropped_outliers, df_classes_dropped_outliers, df_limit_test_classes
 
 
 def get_spreads_per_axis(axis: str) -> pd.DataFrame:
@@ -116,15 +130,133 @@ def save_aligned_swarmplots(x_spreads, y_spreads, z_spreads):
     plt.clf()
 
 
-x_spreads = get_spreads_per_axis('x')
+def sum_values_for_all_axes(experiment_nr, df):
+    df_experiment = df[df.experiment_id == experiment_nr]
+
+    for axis in ['x', 'y', 'z']:
+        sum_vibration_axis_control = []
+        previous_abs_a1 = 0
+        sum_vibration_axis_test = []
+        previous_abs_a2 = 0
+
+        for idx, row in df_experiment.iterrows():
+            column_name = 'a1_' + axis
+            abs_a1 = abs(row[column_name])
+            summed_value_a1 = previous_abs_a1 + abs_a1
+            sum_vibration_axis_control.append(summed_value_a1)
+            previous_abs_a1 = summed_value_a1
+
+            column_name = 'a2_' + axis
+            abs_a2 = abs(row[column_name])
+            summed_value_a2 = previous_abs_a2 + abs_a2
+            sum_vibration_axis_test.append(summed_value_a2)
+            previous_abs_a2 = summed_value_a2
+
+        column_name = 'sum_a1_' + axis
+        df_experiment[column_name] = sum_vibration_axis_control
+
+        column_name = 'sum_a2_' + axis
+        df_experiment[column_name] = sum_vibration_axis_test
+    return df_experiment
+
+
+def get_df_of_first_seconds(df):
+    # isolate first 1.5 seconds with low power
+    condition_seconds = df.timestamp < 5
+    condition_watts = df.hz < 4
+    df_before_powering_up = df[(condition_seconds) & (condition_watts)]
+    return df_before_powering_up
+
+
+def save_plot_differences_vibration_beginnings(axis: str,
+                                                    df: pd.DataFrame,
+                                                    df_classes: pd.DataFrame,
+                                                    experiment_nr: int) -> None:
+
+    experiment = df[df.experiment_id == experiment_nr]
+    status = df_classes[df_classes.index == experiment_nr].status
+    if 1 in status.values:
+        plt.title('good test bearing')
+    else:
+        plt.title('bad test bearing')
+
+    column_name = 'a1_' + axis
+    plt.plot(experiment.timestamp, experiment[column_name], color='green', alpha=0.8, label=('control ' + axis))
+    column_name = 'a2_' + axis
+    plt.plot(experiment.timestamp, experiment[column_name], color='blue', alpha=0.8, label=('test ' + axis))
+    plt.plot(experiment.timestamp, experiment.hz, color='yellow', alpha=0.5, label='hz')
+    plt.legend()
+    savefig_name = 'beginning_vibrations_' + axis + '_exp_nr_' + str(experiment_nr) + '.png'
+    savegif_path = 'visuals\\' + savefig_name
+    plt.savefig(savegif_path)
+    plt.clf()
+
+
+def save_swarmplot_differences_vibration_beginnings(axis: str,
+                                                    df: pd.DataFrame,
+                                                    df_classes: pd.DataFrame,
+                                                    experiment_nr: int) -> None:
+
+    experiment = df[df.experiment_id == experiment_nr]
+    status = df_classes[df_classes.index == experiment_nr].status
+
+    if 1 in status.values:
+        plt.title('good test bearing')
+    else:
+        plt.title('bad test bearing')
+
+    column_name = 'a1_' + axis
+    sns.swarmplot(x=experiment.hz, y=experiment[column_name], color='green', alpha=0.5, dodge=True)
+    column_name = 'a2_' + axis
+    sns.swarmplot(x=experiment.hz, y=experiment[column_name], color='blue', alpha=0.5, dodge=True)
+    plt.legend()
+    plt.ylim([-3, 3])
+    savefig_name = 'swarm_beginning_vibrations_' + axis + '_exp_nr_' + str(experiment_nr) + '.png'
+    savegif_path = 'visuals\\' + savefig_name
+    plt.savefig(savegif_path)
+    plt.clf()
+
+
+###################################################################
+###################################################################
+
+bearings_signals_dropped_outliers, bearing_classes_dropped_outliers, dropped_bearings = \
+    split_dataframes_to_drop_hertz_outliers(bearing_signals, bearing_classes)
+
+# add columns with vibrations sums for all axes for plotting over the whole experiment (not just the beginning seconds)
+good_bearing_101 = sum_values_for_all_axes(101, bearings_signals_dropped_outliers)
+bad_bearing_98 = sum_values_for_all_axes(98, bearings_signals_dropped_outliers)
+
+bearings_signals_experiments_beginnings = get_df_of_first_seconds(bearings_signals_dropped_outliers)
+
+
+# save_plot_differences_vibration_beginnings('x',
+#                                            bearings_signals_experiments_beginnings,
+#                                            bearing_classes_dropped_outliers,
+#                                            98)    # bad bearing
+# save_plot_differences_vibration_beginnings('x',
+#                                            bearings_signals_experiments_beginnings,
+#                                            bearing_classes_dropped_outliers,
+#                                            101)    # good bearing
+# save_swarmplot_differences_vibration_beginnings('x',
+#                                                 bearings_signals_experiments_beginnings,
+#                                                 bearing_classes_dropped_outliers,
+#                                                 98)    # bad
+# save_swarmplot_differences_vibration_beginnings('x',
+#                                                 bearings_signals_experiments_beginnings,
+#                                                 bearing_classes_dropped_outliers,
+#                                                 101)    # good
+
+
+# x_spreads = get_spreads_per_axis('x')
 # write_to_csv(x_spreads, 'x')
 # save_boxplot(x_spreads, 'x')
 
-y_spreads = get_spreads_per_axis('y')
+# y_spreads = get_spreads_per_axis('y')
 # write_to_csv(y_spreads, 'y')
 # save_boxplot(y_spreads, 'y')
 
-z_spreads = get_spreads_per_axis('z')
+# z_spreads = get_spreads_per_axis('z')
 # write_to_csv(z_spreads, 'z')
 # save_boxplot(z_spreads, 'z')
 
